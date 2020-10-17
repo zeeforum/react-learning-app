9c3b092385788655c4f651db51befabb293c83ae
zeeforum <zartashzulfiqar@gmail.com>
2020-10-17 15:50:10 +0500

    Updates

---
+++ precache-manifest.298402f39710d5e0234118f54a0209ed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50c2e3761cbbe719719223392dfbec0",
+    "url": "/react-learning-app/index.html"
+  },
+  {
+    "revision": "6394c22f7130c92c8f4e",
+    "url": "/react-learning-app/static/css/2.2dc7cc20.chunk.css"
+  },
+  {
+    "revision": "4c52c28a7480e5576ee6",
+    "url": "/react-learning-app/static/css/main.92a4f920.chunk.css"
+  },
+  {
+    "revision": "6394c22f7130c92c8f4e",
+    "url": "/react-learning-app/static/js/2.0cc0df19.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/react-learning-app/static/js/2.0cc0df19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c52c28a7480e5576ee6",
+    "url": "/react-learning-app/static/js/main.10400cc8.chunk.js"
+  },
+  {
+    "revision": "47d283a19aba699457bc",
+    "url": "/react-learning-app/static/js/runtime-main.b9e85001.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-learning-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-learning-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-learning-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-learning-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-learning-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
